9f696bf622a08f7ea2f1936e96b3c798eee8dfc6
Gerald Col <gnut@gnut.eu>
2021-03-29 12:45:38 +0200

    Mis à jour articles.php

--- articles.php
+++ articles.php
@@ -1,4 +1,18 @@
 <?php 
+	// On va chercher les articles dans la base
+	// On se connecte à la base
+	require_once 'includes/connect.php';
+
+	// On écrit la requête
+	$sql = "SELECT * FROM `articles` ORDER BY `created_at` DESC";
+
+	// On exécute la requête
+	$requete = $db->query($sql);
+
+	// On récupère leqs données
+	$articles = $requete->fetchAll();
+
+
 	// On definit le titre
 	$titre = "Liste des articles";
 
@@ -11,12 +25,17 @@
 
 	//  On écrit le contenu de la page
 ?>
-
-
- <p>Liste des articles</p>
-
-<?php 
-
+<h1>Liste des articles</h1>
+<section>
+	 <?php foreach($articles as $article):  ?>
+		<article>
+			<h1><a href="article.php?id=<?= $article["id"]?>"><?= strip_tags($article["title"])  ?></a></h1>
+			<p>	Publié le <?= $article["created_at"]?></p>
+			<div><?= strip_tags($article["content"])  ?></div>
+		</article>
+	<?php endforeach; ?>
+</section>
+	<?php
 	// On inclut le footer
 	include 'includes/footer.php';
 	 ?>
